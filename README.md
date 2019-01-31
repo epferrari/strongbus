b66fa04d94e2aa7acac49a65a05aeafa3afc6961
Ethan Ferrari <github@ethanferrari.com>
2019-01-31 00:08:48 -0600

    update docs and add README with link to documentation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Strongbus
+
+A strongly typed, battle tested message bus built for complex event-driven applications.
+
+[API Documentation](https://epferrari.github.io/strongbus)
--- src/types/options.ts
+++ src/types/options.ts
@@ -14,10 +14,10 @@ export interface ListenerThresholds {
 }
 
 /**
- * @prop allowUnhandledEvents `true` - should the Bus throw an error when an event is emitted and there are no listeners
- * @prop name `"Anonymous"` - a name for the bus. included in warn/info messages and errors thrown
+ * @prop allowUnhandledEvents `true` - Should the Bus throw an error when an event is emitted and there are no listeners for the event
+ * @prop name `"Anonymous"` - A name for the bus. Included in warn/info/error potential memory leak messages and unhandled event errors thrown
  * @prop thresholds [[ListenerThresholds]]
- * @prop logger [[Logger]] [console] - how to log potential memory leaks, if thresholds are < Infinity
+ * @prop logger [[Logger]] [`console`] - How to log potential memory leaks, if thresholds are < Infinity
  */
 export interface Options {
   allowUnhandledEvents?: boolean;
