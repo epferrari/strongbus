a05ff0b464420d18ec6f71f0156462823b3290cc
Ethan Ferrari <eferrari@drwholdings.com>
2024-10-18 10:52:50 -0500

    Revert "Revert "void events will only accept no args, null, or undefined""
    This reverts commit 193a60f6677d08de48fe457de698c23bf25c6ec6.

--- src/strongbus.ts
+++ src/strongbus.ts
@@ -111,7 +111,9 @@ export class Bus<TEventMap extends Events.EventMap = Events.EventMap> implements
    */
   protected handleUnexpectedEvent<T extends EventKeys<TEventMap>>(
     event: T,
-    ...payload: TEventMap[T] extends void ? any[] : [TEventMap[T]]
+    ...payload: TEventMap[T] extends void
+      ? ([] | [null] | [undefined])
+      : [TEventMap[T]]
   ) {
     const errorMessage = [
       `Strongbus.Bus received unexpected message type '${String(event)}' with contents:`,
@@ -138,7 +140,9 @@ export class Bus<TEventMap extends Events.EventMap = Events.EventMap> implements
 
   public emit<T extends EventKeys<TEventMap>>(
     event: T,
-    ...payload: TEventMap[T] extends void ? any[] : [TEventMap[T]]
+    ...payload: TEventMap[T] extends void
+      ? ([] | [null] | [undefined])
+      : [TEventMap[T]]
   ): boolean {
     if(event === Events.WILDCARD) {
       throw new Error(`Do not emit "${String(event)}" manually. Reserved for internal use.`);
